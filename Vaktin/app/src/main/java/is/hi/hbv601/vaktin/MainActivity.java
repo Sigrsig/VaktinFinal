f797972c22dd562067668c30cb2d3ebf34e81c5b
Sospsig <sos42@hi.is>
2020-02-24 10:03:59 +0000

    Front page and menu added - Sigga

--- Vaktin/app/src/main/java/is/hi/hbv601/vaktin/MainActivity.java
+++ Vaktin/app/src/main/java/is/hi/hbv601/vaktin/MainActivity.java
@@ -1,20 +1,40 @@
 package is.hi.hbv601.vaktin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import is.hi.hbv601.vaktin.fragments.Kvoldvakt;
+import is.hi.hbv601.vaktin.fragments.Morgunvakt;
+import is.hi.hbv601.vaktin.fragments.Naeturvakt;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button mloginNavButton;
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mloginNavButton = (Button) findViewById(R.id.loginNav);
+        //mloginNavButton = (Button) findViewById(R.id.loginNav);
         /*mloginNavButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +43,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
             }
         });*/
-        mloginNavButton.setOnClickListener(this);
+        //mloginNavButton.setOnClickListener(this);
+
+        // Current date added
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+        TextView twDate = findViewById(R.id.currDate);
+        twDate.setText(currentDate);
+
+        List<Fragment> list = new ArrayList<>();
+        list.add(new Morgunvakt());
+        list.add(new Kvoldvakt());
+        list.add(new Naeturvakt());
+
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new com.example.frontpage3.SlidePagerAdapter(getSupportFragmentManager(), list);
+
+        pager.setAdapter(pagerAdapter);
+
     }
 
     @Override
@@ -35,6 +72,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(new Intent(this, LoginActivity.class));
             break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.edit:
+                Toast.makeText(this, "Edit Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.logout:
+                Toast.makeText(this, "Log In Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.login_Button:
+                Toast.makeText(this, "Log Out Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
 
---
+++ Vaktin/app/src/main/java/is/hi/hbv601/vaktin/fragments/Morgunvakt.java
@@ -0,0 +1,28 @@
+package is.hi.hbv601.vaktin.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import is.hi.hbv601.vaktin.R;
+
+public class Morgunvakt extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.morgunvakt,
+                container,
+                false);
+
+
+        return rootView;
+    }
+}
