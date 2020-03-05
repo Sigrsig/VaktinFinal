1dd85bf09752765bed4e6e60b76e8c7506454303
Loftur Einarsson <loe3@hi.is>
2020-03-05 10:01:16 +0000

    workstation front page. þarf að finna út hvernig við gerum templates í xml

---
+++ Vaktin/app/src/main/java/is/hi/hbv601/vaktin/WorkstationsFrontPageActivity.java
@@ -0,0 +1,19 @@
+package is.hi.hbv601.vaktin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import is.hi.hbv601.vaktin.R;
+
+public class WorkstationsFrontPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workstations);
+
+    }
+
+
+}
