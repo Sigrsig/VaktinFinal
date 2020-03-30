85330189a961b7315b8bf00d7b1255bbdbdcdf01
Loftur Einarsson <loe3@hi.is>
2020-03-30 13:14:21 +0000

    breytti pom.xml

--- VaktinVefapp/target/classes/data.sql
+++ VaktinVefapp/target/classes/data.sql
@@ -1,15 +1,1 @@
-DROP TABLE IF EXISTS billionaires;
-
-CREATE TABLE billionaires (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    career VARCHAR(100) DEFAULT NULL
-);
-
-INSERT INTO billionaires (first_name, last_name, career) VALUES
-    ('Aliko', 'Dangote', 'Billionaire Industrialist'),
-    ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-    ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
-
 
